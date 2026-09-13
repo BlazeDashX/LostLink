dd823e2f44402809b153ae004edd8122eb780ef1
BlazeDashX <refat00021@gmail.com>
2026-09-14 05:53:52 +0600

    feat(claims): add claim API routes and database queries

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
@@ -7,11 +8,15 @@ const {
   createUser,
 } = require("./data/userStore");
 
+const claimsRoutes = require("./routes/claims.routes");
+
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
+
+// Mount API Routes
+app.use("/api/claims", claimsRoutes);
 
 app.get("/", (req, res) => {
   res.json({
